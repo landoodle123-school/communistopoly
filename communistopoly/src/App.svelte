003881bfeeb74b3f6e803a6daea3a2e6f9809ebe
landoodle123-school <29kazenl@wiltonps.org>
2025-01-14 19:01:29 +0000

    created svelte app, prepared to make site

---
+++ communistopoly/src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1>Welcome to Communistopoly</h1>
+  <p>This site is currenty under construction. Please be patient while I work on it.</p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
